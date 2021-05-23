c12cc42010dd608ba5d3a0bfa3985a5e3d7ce263
Ashish V Nair <ashishnair.oph@gmail.com>
2021-05-23 14:35:35 +0530

    INIT

---
+++ InfiniteHealth/Class1.cs
@@ -0,0 +1,34 @@
+﻿using BepInEx;
+using HarmonyLib;
+using UnityEngine;
+
+namespace ValheimModifications
+{
+    [BepInPlugin("comoratehiruki.InfiniteHealth", "Infinite Health", "1.0.0")]
+    [BepInProcess("valheim.exe")]
+    public class InfiniteHealth : BaseUnityPlugin
+    {
+        private readonly Harmony harmony = new Harmony("comoratehiruki.InfiniteHealth");
+        
+
+
+        void Awake()
+        {
+            harmony.PatchAll();
+        }
+
+        [HarmonyPatch(typeof(Character), nameof(Character.SetHealth))]
+        class Health_Patch
+        {
+            static void Prefix(ref float health, Character __instance)
+            {
+                if (health <= 0f && __instance.IsPlayer())
+                {
+                    Debug.Log($"Current Health: {health}");
+                    health = 25f;
+                    Debug.Log($"Modified Base Health: {health}");
+                }
+            }
+        }
+    }
+}
